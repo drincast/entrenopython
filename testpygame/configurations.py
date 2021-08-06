9a2986c4fa6a572acec205b5f399ff60bf3e132e
drincastX <drincast@gmail.com>
2021-08-06 17:28:14 -0500

    Se agrega enemigos, manejo de FPS, archivo de codigo para configuraciones generales

---
+++ testpygame/configurations.py
@@ -0,0 +1,9 @@
+#CONSTANTS
+PATH_RES_IMG = "res/img"
+PATH_RES_SOUND = 'res/sound'
+
+#Variables de configuracion
+GAME_FPS = 30
+gameFPSClock = None
+screenHeight = 600
+screenWidth = 800
--- testpygame/testpygame.py
+++ testpygame/testpygame.py
@@ -1,32 +1,33 @@
-import platform
+import platform, sys
 import math
 import random
 
 import pygame
 from pygame import mixer
 
-#CONSTANTS
-PATH_RES_IMG = "res/img"
-PATH_RES_SOUND = 'res/sound'
+import configurations
 
 #initialize pygame
 pygame.init()
-pygame.font.init()
+#pygame.font.init()
+
+#configurations
+configurations.gameFPSClock = pygame.time.Clock()
 
 #create the screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((configurations.screenWidth, configurations.screenHeight))
 
 #load image
-background = pygame.image.load(PATH_RES_IMG + '/estrella-en-el-espacio.jpg')
-enemy = pygame.image.load('res/img/enemy.png')
+background = pygame.image.load(configurations.PATH_RES_IMG + '/estrella-en-el-espacio.jpg')
+enemy = pygame.image.load(configurations.PATH_RES_IMG + '/enemy.png')
 
 #load sound
-mixer.music.load('res/sound/background.wav')
+mixer.music.load(configurations.PATH_RES_SOUND + '/background.wav')
 mixer.music.play(-1)
 
 #caption icon
 pygame.display.set_caption("Space Invader")
-icon = pygame.image.load(PATH_RES_IMG + '/ufo.png')
+icon = pygame.image.load(configurations.PATH_RES_IMG + '/ufo.png')
 pygame.display.set_icon(icon)
 
 #text score
@@ -39,16 +40,29 @@ textY = 10
 
 
 #player
-playerImg = pygame.image.load(PATH_RES_IMG + "/player.png")
+playerImg = pygame.image.load(configurations.PATH_RES_IMG + "/player.png")
 playerX = 370
 playerY = 480
 playerX_change = 0
 
-#varible configuration
-xInitPositionEnemy = 34
-xEndPositionEnemy = 740
-yInitPositionEnemy = 4
-flagRow = 0
+#configuration Enemies
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+def CreateEnemies():
+    for i in range(num_of_enemies):
+        enemyImg.append(pygame.image.load(configurations.PATH_RES_IMG + '/enemy.png'))
+        enemyX.append(random.randint(0, 736))
+        enemyY.append(random.randint(50, 150))
+        enemyX_change.append(4)
+        enemyY_change.append(40)
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -73,6 +87,11 @@ def show_score(font):
 
 #function for spawn the enemies ver 01
 def SpawnEnemies01(xInitPositionEnemy, xEndPositionEnemy, yInitPositionEnemy, flagRow):
+    #varible configuration
+    # xInitPositionEnemy = 34
+    # xEndPositionEnemy = 740
+    # yInitPositionEnemy = 4
+    # flagRow = 0
     for y in range(yInitPositionEnemy, 272, 72):
         for x in range(xInitPositionEnemy, xEndPositionEnemy, 74):
             screen.blit(enemy, (x, y))
@@ -96,14 +115,55 @@ while running:
     #put backgroud image
     screen.blit(background, (0, 0))        
 
-    SpawnEnemies01(34, 740, 4, 0)
+    # SpawnEnemies01(34, 740, 4, 0)
+    CreateEnemies()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        #key is pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+            elif event.key == pygame.K_RIGHT:
+                playerX_change = 5
+            elif event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+                    bulletSound = mixer.Sound(configurations.PATH_RES_SOUND + "/laser.wav")
+                    bulletSound.play()
+                    # get the current x coordinate of ship
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+            
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 770:
+        playerX = 770
+
+    #enemy movement
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i] 
+
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 750:
+            enemyX_change[i] = -4
+            enemyY[i] += enemyY_change[i]
+
+        enemy(enemyX[i], enemyY[i], i)
+
             
     screen.blit(show_score(font), (10, 10))
 
     player(playerX, playerY)
     pygame.display.update()
+    configurations.gameFPSClock.tick(configurations.GAME_FPS)
 
