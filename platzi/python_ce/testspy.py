ff6602e18723789c45a3922e698074575d0bc43f
drincastX <drincast@gmail.com>
2024-09-28 16:57:08 -0500

    se sube carpeta y archivos para practicas para el curso de paython de platzy

---
+++ platzi/python_ce/testspy.py
@@ -0,0 +1,20 @@
+phrase = "Nunca pares de aprender"
+val_numeric = 3.14159265358979323846
+
+print("Nunca", "pares", "de", "aprender")
+print("Nunca", "pares", 'de', 'aprender', sep="-", end=" other ")
+print("Nunca", "pares", 'de', 'aprender', sep="-")
+
+print(f"{phrase}")
+print("La frase = {}".format(phrase))
+
+print("Valor = {}".format(val_numeric))
+print("Valor = {:.4f}".format(val_numeric))
+
+print('\'Soy el mejor aprendiendo\'')
+
+name = input("Su nombre: ")
+age = int(input("Tu edad: "))
+
+print(f"Nombre: {name} - {age} años")
+
