cc4d7afac4d8eb763cc814f5b5fe005e2b897e79
drincastX <drincast@gmail.com>
2025-03-30 17:37:33 -0500

    Se agrega código l ejecicio de concesionario de carros, se mejora la funcionalidad de compra y venta, y ya improme lista de carros

--- platzi/python_ce/023POO/concessionaire.py
+++ platzi/python_ce/023POO/concessionaire.py
@@ -1,3 +1,6 @@
+from tabulate import tabulate
+
+############## Class definitons 
 class Vehicle:
     def __init__(self, available, brand, condition, color, name, price, type, ):
         self.available = available
@@ -19,23 +22,43 @@ class User:
 
         self.purchased_cars = []
 
-    def buy_car(self, car):
-        if car.available:
-            if self.balance >= car.price:
-                car.sold()
-                self.purchased_cars.append(car)
-                print(f"Compraste El vehículo {car.name} - {car.brand} por {car.sold}")
+    def buy_car(self, car, concessionaire):
+        try:
+            if car.available:
+                if self.balance >= car.price:
+                    self.balance -= car.price
+                    concessionaire.balance += car.price
+                    concessionaire.cars_sale.remove(car)
+                    car.sold()                
+                    self.purchased_cars.append(car)
+                    print(f"{self.name} Compraste El vehículo {car.name} - {car.brand} por {car.price}")
+                else:
+                    raise ValueError("{self.name} no tienes fondos suficientes para comprarlo, estudia en PLATZI para ganar masssss :)")
             else:
-                raise ValueError("No tienes fondos suficientes para comprarlo, estudia en PLATZI para ganar masssss :)")
-        else:
-            print(f"El vehículo {car.name} - {car.brand} No esta disponible")
+                print(f"{self.name} el vehículo {car.name} - {car.brand} No esta disponible")
+        except Exception as ex:
+            print(ex)        
+
+    def show_user_data(self):
+        print(f"Nombre: {self.name}")
+        print(f"Balance: {self.balance}")
+        car_list = tool_get_car_list(self.purchased_cars)
+        print(tabulate(car_list
+                       , headers=["Disponible", "Marca", "Color", "Nombre", "Precio", "Tipo", "Condición"]
+                       , tablefmt="pretty"))
 
     def sell_car(self, car, concessionaire):
         sale_price = (car.price - (car.price*0.1))
         self.balance = self.balance + sale_price
         car.price = sale_price
-        concessionaire.buy_used_car(car)            
+        concessionaire.buy_used_car(car)
+        self.purchased_cars.remove(car)
         print(f"Has vendido el {car.name} - {car.brand} a {sale_price}, felicidades o ¿no?")
+
+    def show_my_cars(self):
+        print(f"Los carros de {self.name}")
+        car_list = tool_get_car_list(self.purchased_cars)
+        print(f"carriñios {self.purchased_cars}")
 
 class Concessionaire:
     def __init__(self, balance, name):
@@ -53,7 +76,25 @@ class Concessionaire:
     def buy_used_car(self, car):
         profit_price = car.price + (car.price * 0.05)
         car.price = profit_price
-        self.add_car(car)
+        self.add_car(car)        
+
+    def show_cars(self):
+        print(f"Concesionario {self.name}")
+        print(f"Carriñios a la venta")
+        car_list = tool_get_car_list(self.cars_sale)
+        print(tabulate(car_list
+                       , headers=["Disponible", "Marca", "Color", "Nombre", "Precio", "Tipo", "Condición"]
+                       , tablefmt="pretty"))
+
+############## Funtions definitons 
+
+def tool_get_car_list(object):
+    try:
+        car_list = [[c.available, c.brand, c.color, c.name, c.price, c.type, c.condition] for c in object]
+        return car_list
+    except Exception as ex:
+        print(ex)
+    
 
 #############################################################
 
@@ -78,4 +119,51 @@ user3 = User(100000, "Charlie", 3)
 # Crear un objeto Concessionaire
 concessionaire1 = Concessionaire(100000, "AutoIluminyMax")
 
+concessionaire1.cars_sale.append(vehicle1)
+concessionaire1.cars_sale.append(vehicle2)
+concessionaire1.cars_sale.append(vehicle3)
+concessionaire1.cars_sale.append(vehicle4)
+concessionaire1.cars_sale.append(vehicle5)
+concessionaire1.cars_sale.append(vehicle6)
+concessionaire1.cars_sale.append(vehicle7)
+concessionaire1.cars_sale.append(vehicle8)
+concessionaire1.cars_sale.append(vehicle9)
+concessionaire1.cars_sale.append(vehicle10)
 
+print("___________________________________________________________________________________")
+print(f"Balance: {concessionaire1.balance}")
+concessionaire1.show_cars()
+
+print("\n Compradores ...")
+print(vars(user1))
+print(vars(user2))
+print(vars(user3))
+
+#print(user1.get_dict_show_user_data(user1))
+
+user1.buy_car(vehicle1, concessionaire1)
+user2.buy_car(vehicle2, concessionaire1)
+user3.buy_car(vehicle3, concessionaire1)
+
+print("\n Compradores ...")
+print("--------------------------------------------------------------------------------")
+user1.show_user_data()
+user2.show_user_data()
+user3.show_user_data()
+
+print("\n concesionario ...")
+print(f"Balance: {concessionaire1.balance}")
+concessionaire1.show_cars()
+
+print("\n usuario vende ...")
+user1.sell_car(vehicle1, concessionaire1)
+
+print("\n Compradores ...")
+print("--------------------------------------------------------------------------------")
+user1.show_user_data()
+
+print("\n concesionario ...")
+print(f"Balance: {concessionaire1.balance}")
+concessionaire1.show_cars()
+
+print("___________________________________________________________________________________")
